2fd614aff3250e9207d49c72ca71a2a1116fd6cd
Michael <dattt.piass@gmail.com>
2023-06-26 12:31:16 +0700

    EC2 - Working 80%

--- project-0/ec2/07-ec2-private.tf
+++ project-0/ec2/07-ec2-private.tf
@@ -1,13 +1,33 @@
-module "ec2_private" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.1.0"
+locals {
+  multiple_instances = {
+    one = {
+      availability_zone = element(module.vpc.azs, 0)
+      subnet_id         = element(module.vpc.private_subnets, 0)
+    }
+    two = {
+      availability_zone = element(module.vpc.azs, 1)
+      subnet_id         = element(module.vpc.private_subnets, 1)
+    }
+    three = {
+      availability_zone = element(module.vpc.azs, 2)
+      subnet_id         = element(module.vpc.private_subnets, 2)
+    }
+  }
+}
 
-  depends_on    = [module.vpc]
-  name          = "${local.name}-ec2-private"
+
+module "ec2_private" {
+  source     = "terraform-aws-modules/ec2-instance/aws"
+  version    = "5.1.0"
+  depends_on = [module.vpc]
+
+  for_each = local.multiple_instances
+
+  name          = "${local.name}-ec2-private-${each.key}"
   ami           = data.aws_ami.al2023.id
   instance_type = var.private_instance_type
   key_name      = var.instance_keypair
-  subnet_id     = module.vpc.private_subnets[0]
+  #subnet_id     = module.vpc.private_subnets[0]
   #subnet_ids             = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
   vpc_security_group_ids = [module.sg_ec2private.security_group_id]
   root_block_device = [
